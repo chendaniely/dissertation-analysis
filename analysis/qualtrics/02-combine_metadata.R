38dc4701ec69be40cecfc70ccf97d14804891dcd
Daniel Chen <chendaniely@gmail.com>
2020-08-04 12:42:14 -0400

    add space for style guide

--- analysis/qualtrics/02-combine_metadata.R
+++ analysis/qualtrics/02-combine_metadata.R
@@ -7,7 +7,7 @@ library(rvest)
 
 
 strip_html <- function(s) {
-  if (stringr::str_detect(s, "<.*?>")){
+  if (stringr::str_detect(s, "<.*?>")) {
     return(rvest::html_text(xml2::read_html(s)))
   } else {
     return(s)
