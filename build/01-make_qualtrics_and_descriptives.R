b055aaee65ab1097f034a741547c224a0f1d6967
Daniel Chen <chendaniely@gmail.com>
2020-08-18 14:30:13 -0400

    rename files, fix build pipeline script, make clean

--- build/01-make_qualtrics_and_descriptives.R
+++ build/01-make_qualtrics_and_descriptives.R
@@ -6,7 +6,12 @@ library(purrr)
 # Clean -----
 
 analysis_html <- fs::dir_ls(here("analysis"), recurse = TRUE, glob = "*.html")
-purrr::walk(analysis_html, fs::file_delete)
+qualtrics_files <- fs::dir_ls(here("output", "survey", "01-self_assessment"), recurse = TRUE)
+persona_files <- fs::dir_ls(here("output", "persona"), recurse = TRUE)
+
+files_to_delete <- c(analysis_html, qualtrics_files, persona_files)
+
+purrr::walk(files_to_delete, fs::file_delete)
 
 # Qualtrics -----
 source(here("analysis/qualtrics/01-process_qualtrics.R"))
@@ -15,4 +20,15 @@ source(here("analysis/qualtrics/02-combine_metadata.R"))
 rmarkdown::render(
   here("analysis/qualtrics/03-self_assessment_persona_descriptives.Rmd"),
   output_dir = here("./output/survey/01-self_assessment/")
+)
+
+# Persona clusters -----
+rmarkdown::render(
+  here("analysis/persona/01-clustering.Rmd"),
+  output_dir = here("./output/persona/")
+)
+
+rmarkdown::render(
+  here("analysis/persona/02-descriptives_by_group.Rmd"),
+  output_dir = here("./output/persona/")
 )
