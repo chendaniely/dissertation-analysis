03b4e8a116b471119d551e350275831406a45a58
Daniel Chen <chendaniely@gmail.com>
2020-11-11 22:41:02 -0500

    refactor code to handle all 3 surveys and create global unique IDs

--- analysis/01-qualtrics/01-process_qualtrics.R
+++ analysis/01-qualtrics/01-process_qualtrics.R
@@ -3,30 +3,10 @@ library(dplyr)
 library(readr)
 library(here)
 library(fs)
+library(purrr)
+library(glue)
 
-remove_identifiers <- function(dat) {
-  dat <- dat %>%
-    # Why is qualtrics tracking location?
-    dplyr::select(StartDate:Status, Progress:ResponseId, starts_with("Q")) %>% 
-    # drop people who do not want to be in the survey
-    dplyr::filter(Q1.4 == "Yes. I have read the consent form and this response will serve as my consent to participate in the research study.")
-  
-  dat <- dat %>%
-    # drop test data
-    dplyr::filter(StartDate >= "2020-07-23")
-  
-  # check for no duplicate IDs, excluding NAs
-  stopifnot(all(!duplicated(na.omit(dat$Q2.2))))
-  
-  # create unique integer IDs
-  dat <- dat %>%
-    dplyr::mutate(id = 1:nrow(dat)) %>%
-    dplyr::select(-ResponseId, -Q2.2) %>%
-    # re-order columns
-    dplyr::select(id, everything())
-  
-  return(dat)
-}
+source(here("./R/remove_identifiers.R"))
 
 # https://github.com/ropensci/qualtRics
 # QUALTRICS_BASE_URL="virginiatech.ca1.qualtrics.com"
@@ -34,14 +14,51 @@ remove_identifiers <- function(dat) {
 # usethis::edit_r_environ()
 readRenviron("~/.Renviron")
 
+## Get qualtrics surveys -----
+
 surveys <- all_surveys()
+survey_names <- c("persona",
+                  "pre-workshop survey",
+                  "post-workshop survey")
 
-self_assessment_id <-  surveys$id[stringr::str_detect(surveys$name, "student_survey")]
+survey_ids <- purrr::map_chr(survey_names, ~ surveys$id[stringr::str_detect(surveys$name, .)])
 
-survey_self_assessment <- self_assessment_id %>%
-  qualtRics::fetch_survey(surveyID = .) %>%
-  remove_identifiers()
+survey_dfs <- purrr::map(survey_ids,
+                         ~ qualtRics::fetch_survey(surveyID = .,
+                                                   verbose = FALSE) %>%
+                           remove_identifiers()
+                         )
+
+## Recode the unique identifiers -----
+print(purrr::map(survey_dfs, nrow))
+
+all_ids <- purrr::map_df(survey_dfs, dplyr::select, Q2.2)
+print(glue("Total number responses: {nrow(all_ids)}"))
+
+all_ids_unique <- unique(all_ids)
+print(glue("Unique number responses: {nrow(all_ids_unique)}"))
+
+# create unique integer IDs
+ids <- all_ids_unique %>%
+  dplyr::mutate(id = 1:nrow(all_ids_unique))
+
+survey_dfs_deidentified <- purrr::map(survey_dfs,
+                                      ~ dplyr::left_join(., ids, by = "Q2.2") %>%
+                                        dplyr::select(-ResponseId, -Q2.2) %>%
+                                        dplyr::select(id, everything())
+                                        )
+
+## Save out the survey data -----
+
+survey_dfs_deidentified[[2]]
+
+
+survey_save_pths <- c(
+  here("./data/original/surveys/01-01-self_assessment_persona.tsv"),
+  here("./data/original/surveys/01-02-pre_workshop.tsv"),
+  here("./data/original/surveys/01-03-post_workshop.tsv")
+)
 
 fs::dir_create(here("./data/original/surveys"), recurse = TRUE)
-readr::write_tsv(survey_self_assessment, here("./data/original/surveys/01-self_assessment_persona.tsv"))
-#readr::write_tsv(labels, here("./data/original/surveys/01-self_assessment_persona_labels.tsv"))
+
+purrr::walk2(survey_dfs_deidentified, survey_save_pths, readr::write_tsv)
