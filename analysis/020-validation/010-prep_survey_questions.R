571354701850eae6f7ddca82ebed15ab8c1d3b68
Daniel Chen <chendaniely@gmail.com>
2021-10-09 17:04:18 -0400

    fix id -> id_person and other typo code

--- analysis/020-validation/010-prep_survey_questions.R
+++ analysis/020-validation/010-prep_survey_questions.R
@@ -3,7 +3,8 @@ library(glue)
 library(readr)
 library(dplyr)
 library(utf8)
-
+library(stringr)
+library(tidyr)
 
 source(here::here("./analysis/010-qualtrics/question_str_to_int.R"))
 
@@ -30,7 +31,7 @@ persona_wide <- function(dat, qnum = c(3, 4, 5, 6)) {
     # https://stackoverflow.com/questions/46814856/use-gsub-to-replace-curly-apostrophe-with-straight-apostrophe-in-r-list-of-chara
     dplyr::mutate(response = utf8::utf8_normalize(response, map_quote = TRUE)) %>%
     dplyr::mutate(response_numeric = purrr::map2_int(question_part, response, recode_responses_int)) %>%
-    dplyr::select(-response) %>%``
+    dplyr::select(-response) %>%
     tidyr::drop_na() %>%
     tidyr::pivot_wider(names_from = question_part,
                        values_from = response_numeric) %>%
@@ -44,7 +45,7 @@ persona_wide <- function(dat, qnum = c(3, 4, 5, 6)) {
 persona <- readr::read_tsv(here("./data/final/01-surveys/01-self_assessment_persona_with_questions.tsv"))
 
 persona_q_a <- persona %>%
-  dplyr::select(id, question_part, response)
+  dplyr::select(id_person, question_part, response)
   
 wide_survey_only <- persona_wide(persona_q_a, qnum = 3:6)
 wide_survey_likert <- persona_wide(persona_q_a, qnum = 3:7)
